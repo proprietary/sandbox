4aa2c9e9d7c93f7c9ae2d8d60aa26898730c7a7a
Zelly Snyder <zelly@outlook.com>
2020-06-12 02:59:56 -0700

    complete leetcode 58

---
+++ cpp/length_of_last_word.cc
@@ -0,0 +1,27 @@
+/*
+ * Leetcode 58. Length of Last Word
+ */
+#include <gtest/gtest.h>
+
+#include <string>
+
+class Solution {
+ public:
+  int lengthOfLastWord(std::string s) {
+    int length = 0;
+    auto it = s.rbegin();
+    while (it != s.rend() && *it == ' ') {
+      ++it;
+    }
+    for (; it != s.rend() && *it != ' '; ++it) {
+      ++length;
+    }
+    return length;
+  }
+};
+
+TEST(Leetcode, Solution) {
+  Solution solution{};
+  EXPECT_EQ(5, solution.lengthOfLastWord("Hello World"));
+  EXPECT_EQ(1, solution.lengthOfLastWord("a "));
+}
