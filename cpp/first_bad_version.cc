3d01619044a3d2b99ea21db13289a3d55e94b272
Zelly Snyder <zelly@outlook.com>
2020-06-12 02:51:24 -0700

    complete leetcode: first bad version

---
+++ cpp/first_bad_version.cc
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+bool isBadVersion(int version);
+
+class Solution {
+public:
+	int firstBadVersion(int n) {
+		int hi = n;
+		int lo = 1;
+		while (hi > lo) {
+			int mid = lo + (hi - lo) / 2;
+			bool res = isBadVersion(mid);
+			if (res == true) {
+				hi = mid;
+			} else if (res == false) {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+};
+
+static int TEST_BAD_VERSION = 4;
+
+TEST(Leetcode, Solution) {
+	Solution solution {};
+	TEST_BAD_VERSION = 4;
+	EXPECT_EQ(4, solution.firstBadVersion(5));
+	TEST_BAD_VERSION = 1;
+	EXPECT_EQ(1, solution.firstBadVersion(1));
+	TEST_BAD_VERSION = 2;
+	EXPECT_EQ(2, solution.firstBadVersion(3));
+	TEST_BAD_VERSION = 1;
+	EXPECT_EQ(1, solution.firstBadVersion(2));
+	TEST_BAD_VERSION = 1702766719;
+	EXPECT_EQ(1702766719, solution.firstBadVersion(2126753390));
+}
+
+bool isBadVersion(int version) {
+	return version >= TEST_BAD_VERSION;
+}
