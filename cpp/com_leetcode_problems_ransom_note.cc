1586c13b8805dfd8bf4afcbb0e57b5ed184067fb
Zelly Snyder <zelly@outlook.com>
2020-12-04 23:58:38 -0800

    use less memory

--- cpp/com_leetcode_problems_ransom_note.cc
+++ cpp/com_leetcode_problems_ransom_note.cc
@@ -3,17 +3,17 @@
 class Solution {
 public:
 	bool canConstruct(std::string ransomNote, std::string magazine) {
-		constexpr auto num_letters = 255;
+		constexpr auto num_letters = 26;
 		unsigned int letters[num_letters];
 		std::fill(std::begin(letters), std::end(letters), 0);
 		for (auto i = 0; i < num_letters; ++i) {
 			letters[i] = 0;
 		}
 		for (auto const ch : magazine) {
-			letters[ch] += 1;
+			letters[ch - 'a'] += 1;
 		}
 		for (auto const ch : ransomNote) {
-			if (letters[ch]-- == 0) {
+			if (letters[ch - 'a']-- == 0) {
 				return false;
 			}
 		}
