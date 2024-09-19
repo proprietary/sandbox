a395fbe2fbf2a00b2dc55eaf705d68092511b125
Zelly Snyder <zelcon@zelcon.net>
2024-09-18 18:56:30 -0700

    Implement print utility for maps

--- cpp/utils.hpp
+++ cpp/utils.hpp
@@ -5,9 +5,18 @@
 #include <vector>
 
 template <typename T>
-concept HasMemberBeginEnd = requires(T t) {
+concept PrintableArray = requires(T t) {
   { t.begin() } -> std::input_or_output_iterator;
   { t.end() } -> std::sentinel_for<decltype(t.begin())>;
+  { std::cout << *t.begin() };
+};
+
+template <typename T>
+concept PrintableMap = requires(T t) {
+  { t.begin() } -> std::input_or_output_iterator;
+  { t.end() } -> std::sentinel_for<decltype(t.begin())>;
+  { std::cout << t.begin()->first };
+  { std::cout << t.begin()->second };
 };
 
 template <typename T, size_t N>
@@ -30,19 +39,21 @@ void print_2d_array(It begin, It end) {
   std::cout << " ]\n";
 }
 
-template <HasMemberBeginEnd T>
-void print_array(const T& arr) {
-  print_array(arr.begin(), arr.end());
+template <PrintableMap T>
+void print_map(const T& m) {
+  print_array(m.begin(), m.end(), [](const auto x) { return "(" + std::to_string(x->first) + ", " + std::to_string(x->second) + ")"; });
 }
 
-template <typename It>
-void print_array(It begin, It end) {
+template <PrintableArray T>
+void print_array(const T& arr) {
+  print_array(arr.begin(), arr.end(), [](const auto x) { return *x; });
+}
+
+template <typename It, typename F>
+void print_array(It begin, It end, F accessor_fn) {
   std::cout << "[ ";
   for (auto it = begin; it != end; ++it) {
-    std::cout << *it;
-    if (it + 1 != end) {
-      std::cout << ", ";
-    }
+    std::cout << accessor_fn(it) << ", ";
   }
   std::cout << " ]\n";
 }
