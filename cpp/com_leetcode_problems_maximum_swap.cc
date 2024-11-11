f574fe34522f806cbf0deee9938b3ad38a00304f
Zelly Snyder <zelcon@zelcon.net>
2024-11-11 10:31:02 -0800

    Bench and test alternate solution

--- cpp/com_leetcode_problems_maximum_swap.cc
+++ cpp/com_leetcode_problems_maximum_swap.cc
@@ -1,12 +1,39 @@
+#include <benchmark/benchmark.h>
 #include <gtest/gtest.h>
 
 #include <algorithm>
-
-#include "gtest/gtest.h"
+#include <vector>
 
 namespace {
 class Solution {
   public:
+  int maximumSwap2(int num) {
+    std::string n = std::to_string(num);
+    const size_t N = n.size();
+
+    if (N == 1) {
+      return num;
+    }
+
+    // first pass to populate array with indices of max element to the right
+    std::vector<int> max_rtl(N);
+    max_rtl[N - 1] = N - 1;
+    for (int idx = N - 2; idx >= 0; --idx) {
+      max_rtl[idx] = (n[idx] > n[max_rtl[idx + 1]]) ? idx : max_rtl[idx + 1];
+    }
+
+    // second pass to greedily find the maximum swap
+    for (int idx = 0; idx < N; ++idx) {
+      if (n[idx] < n[max_rtl[idx]]) {
+        std::swap(n[idx], n[max_rtl[idx]]);
+        return std::stoi(n);
+      }
+    }
+
+    // no maximum swap found
+    return num;
+  }
+
   int maximumSwap(int num) {
     std::string n = std::to_string(num);
     int best = num;
@@ -36,6 +63,13 @@ TEST_P(ParamTest, Examples) {
   EXPECT_EQ(tv.expected, actual);
 }
 
+TEST_P(ParamTest, Solution2) {
+  auto tv = GetParam();
+
+  auto actual = Solution().maximumSwap2(tv.num);
+  EXPECT_EQ(tv.expected, actual);
+}
+
 static auto TEST_CASES = {
     TV{2736, 7236},
     TV{9973, 9973},
@@ -43,4 +77,30 @@ static auto TEST_CASES = {
 };
 
 INSTANTIATE_TEST_SUITE_P(Suite, ParamTest, ::testing::ValuesIn(TEST_CASES));
+
+static void run_solution1(::benchmark::State& state) {
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(Solution().maximumSwap(state.range(0)));
+  }
+  state.SetComplexityN(state.range(0));
+}
+BENCHMARK(run_solution1)->Range(1, 100000000)->Complexity();
+
+static void run_solution2(::benchmark::State& state) {
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(Solution().maximumSwap2(state.range(0)));
+  }
+  state.SetComplexityN(state.range(0));
+}
+BENCHMARK(run_solution2)->Range(1, 100000000)->Complexity();
+
 }  // namespace
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::benchmark::Initialize(&argc, argv);
+
+  ::benchmark::RunSpecifiedBenchmarks();
+
+  return RUN_ALL_TESTS();
+}
